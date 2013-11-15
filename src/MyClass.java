f673d3443990818810f040e329718a75f6d1ff13
teamAEngagePoint <kondr_vlad@mail.ru>
2013-11-15 11:36:34 +0200

    Master Commit

---
+++ src/MyClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: v.kondratenko
+ * Date: 11/15/13
+ * Time: 11:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyClass {
+}
